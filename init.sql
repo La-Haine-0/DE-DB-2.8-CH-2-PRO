b41b71e63e4fab43599d3a875ed7e3aee697db87
La-Haine-0 <ghonchar00@mail.ru>
2023-10-29 23:13:01 +0300

    fix old

--- init.sql
+++ init.sql
@@ -1,10 +1,9 @@
 -- Создание таблицы для хранения данных о продажах товаров.
 CREATE TABLE IF NOT EXISTS sales (
-    `date` Date,
+    date Date,
     category String,
     product String,
     sales_amount Float32,
     customer_id UInt32
 ) ENGINE = MergeTree()
-ORDER BY `date`;
-
+ORDER BY date;
